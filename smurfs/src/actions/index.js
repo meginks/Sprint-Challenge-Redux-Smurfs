339055e969a1602c35cfdd3230eafe6d65597ebc
meginks <meganjones.dev@gmail.com>
2019-03-15 08:13:55 -0700

    added action creators

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,7 +1,16 @@
+import axios from 'axios';
+
 /* 
   Action Types Go Here!
   Be sure to export each action type so you can pull it into your reducer
 */
+const ADD_SMURF = 'ADD_SMURF';
+const GET_SMURFS = 'GET_SMURFS';
+const UPDATE_SMURF = 'UPDATE_SMURF';
+const DELETE_SMURF = 'DELETE_SMURF';
+
+const SUCCESS = 'SUCCESS';
+const FAILURE = 'FAILURE';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -13,3 +22,22 @@
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurf = () = dispatch => {
+  dispatch({ GET_SMURFS }) 
+  axios 
+  .get('http://localhost:3333/smurfs/')
+  .then(res => {
+    console.log("res",res);
+    dispatch({ 
+      type: SUCCESS, 
+      payload: res.data.smurfs
+    })
+  })
+  .catch(err => {
+    dispatch({
+      type: FAILURE, 
+      payload: err.response.data.error.message
+    })
+  })
+}; 
